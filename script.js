034ff0e98cec9a7047b4ac4605dbebcee57e16cf
mangoseeds <juyonbae@gmail.com>
2023-12-21 23:45:59 +0900

    nav bar fixed

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const startPage = document.getElementById("start_page");
-const quizPage = document.getElementById("quiz_page");
-const resultPage = document.getElementById("result_page");
-
+// const startPage = document.getElementById("start_page");
+// const quizPage = document.getElementById("quiz_page");
+// const resultPage = document.getElementById("result_page");
+let startButton = document.getElementById("start-button");
+let shareButton = document.getElementById("share");
 let num = 1;
 const q = {
     1: {
@@ -253,6 +254,7 @@ function copyURI(evt) {
     navigator.clipboard.writeText(evt.target.getAttribute("href")).then(
         () => {
             /*clipboard successfully set */
+            alert("링크가 클립보드에 복사되었습니다.")
         },
         () => {
             /*clipboard set failed */
@@ -260,110 +262,112 @@ function copyURI(evt) {
     );
 }
 
-document.addEventListener("DOMContentLoaded", function (){
-    startPage.style.visibility = 'visible';
-    quizPage.style.visibility = 'hidden';
-    resultPage.style.visibility = 'hidden';
-    document.getElementById("start-button").
+document.addEventListener("DOMContentLoaded", function () {
 
-)};
-$(document).ready(function () {
-    $(".start_page").show();
-    $(".quiz_page").hide();
-    $(".result_page").hide();
-
-    $("#start-button").hover(
-        function () {
-            $(this).css("background-color", "#bd1111");
-            $(this).css("color", "#185033");
-            $(this).css("border", "3px solid #bd1111");
-        },
-        function () {
-            $(this).css("background-color", "#185033");
-            $(this).css("color", "#bd1111");
-            $(this).css("border", "3px solid #bd1111");
-        }
-    );
-});
-
-//hide start_page, show quiz_page
-function start() {
-    $(".start_page").hide();
-    $(".quiz_page").show();
-    next();
-}
-
-$("#A").click(function () {
-    var type = $("#type").val();
-    var preValue = $("#" + type).val();
-    $("#" + type).val(parseInt(preValue) + 1);
-    next();
-});
-$("#B").click(function () {
-    next();
-});
-
-$("#A").hover(
-    function () {
-        $(this).css("background-color", q[num]["c2"]);
-        $(this).css("color", q[num]["c1"]);
-        $(this).css("border", "3px solid" + q[num]["c2"]);
-    },
-    function () {
-        $(this).css("background-color", q[num - 1]["c2"]);
-        $(this).css("color", q[num - 1]["c1"]);
-        $(this).css("border", "3px solid" + q[num - 1]["c1"]);
-    }
-);
-$("#B").hover(
-    function () {
-        $(this).css("background-color", q[num]["c2"]);
-        $(this).css("color", q[num]["c1"]);
-        $(this).css("border", "3px solid" + q[num]["c2"]);
-    },
-    function () {
-        $(this).css("background-color", q[num - 1]["c2"]);
-        $(this).css("color", q[num - 1]["c1"]);
-        $(this).css("border", "3px solid" + q[num - 1]["c1"]);
+        startButton.addEventListener("click", function () {
+            window.location.href = 'quiz.html';
+        });
+        shareButton.addEventListener("click", copyURI);
     }
 );
 
-function next() {
-    if (num == 13) {
-        // let colors = ['#914E72', '#0078BF', '#00A95C', '#3255A4', '#3D5588', '#00838A', '#BB8B41', '#407060', '#FF665E', '#925F52', '#FFE800', '#FF6C2F', '#FF48B0', '#AC936E', '#67B346', '#009DA5', '#BB76CF', '#FFB511', '#928D88', '#5EC8E5', '#82D8D5', '#FFE900', '#FF4C65', '#44D62C', '#FFFFFF']
-        // const choose = colors[Math.floor(Math.random() * colors.length)]
-
-        $(".quiz_page").hide();
-        $(".result_page").show();
-
-        let mbti = "";
-
-        $("#EI").val() < 2 ? (mbti += "I") : (mbti += "E");
-        $("#SN").val() < 2 ? (mbti += "N") : (mbti += "S");
-        $("#TF").val() < 2 ? (mbti += "F") : (mbti += "T");
-        $("#JP").val() < 2 ? (mbti += "P") : (mbti += "J");
-
-        $("#img").attr("src", result[mbti]["img"]);
-        $("#result").html(result[mbti]["result"]);
-        $("#explain").html(result[mbti]["explain"]);
-        $("#extra").html(result[mbti]["extra"]);
-        $("#gift").html(result[mbti]["gift"]);
-    } else {
-        //문제 & 선택지
-        $("#question").html(q[num]["q"]);
-        $("#type").val(q[num]["type"]);
-        $("#A").html(q[num]["A"]);
-        $("#B").html(q[num]["B"]);
-        //색 세팅
-        $(".quiz_page").attr("style", "background-color: " + q[num]["c1"]);
-        $("#question").css("color", q[num]["c2"]);
-        $("#A").css("background-color", q[num]["c2"]);
-        $("#A").css("color", q[num]["c1"]);
-        $("#A").css("border", "3px solid" + q[num]["c1"]);
-        $("#B").css("background-color", q[num]["c2"]);
-        $("#B").css("color", q[num]["c1"]);
-        $("#B").css("border", "3px solid" + q[num]["c1"]);
-
-        num++;
-    }
-}
+// $(document).ready(function () {
+//     $(".start_page").show();
+//     $(".quiz_page").hide();
+//     $(".result_page").hide();
+//
+//     $("#start-button").hover(
+//         function () {
+//             $(this).css("background-color", "#bd1111");
+//             $(this).css("color", "#185033");
+//             $(this).css("border", "3px solid #bd1111");
+//         },
+//         function () {
+//             $(this).css("background-color", "#185033");
+//             $(this).css("color", "#bd1111");
+//             $(this).css("border", "3px solid #bd1111");
+//         }
+//     );
+// });
+//
+// //hide start_page, show quiz_page
+// function start() {
+//     $(".start_page").hide();
+//     $(".quiz_page").show();
+//     next();
+// }
+//
+// $("#A").click(function () {
+//     var type = $("#type").val();
+//     var preValue = $("#" + type).val();
+//     $("#" + type).val(parseInt(preValue) + 1);
+//     next();
+// });
+// $("#B").click(function () {
+//     next();
+// });
+//
+// $("#A").hover(
+//     function () {
+//         $(this).css("background-color", q[num]["c2"]);
+//         $(this).css("color", q[num]["c1"]);
+//         $(this).css("border", "3px solid" + q[num]["c2"]);
+//     },
+//     function () {
+//         $(this).css("background-color", q[num - 1]["c2"]);
+//         $(this).css("color", q[num - 1]["c1"]);
+//         $(this).css("border", "3px solid" + q[num - 1]["c1"]);
+//     }
+// );
+// $("#B").hover(
+//     function () {
+//         $(this).css("background-color", q[num]["c2"]);
+//         $(this).css("color", q[num]["c1"]);
+//         $(this).css("border", "3px solid" + q[num]["c2"]);
+//     },
+//     function () {
+//         $(this).css("background-color", q[num - 1]["c2"]);
+//         $(this).css("color", q[num - 1]["c1"]);
+//         $(this).css("border", "3px solid" + q[num - 1]["c1"]);
+//     }
+// );
+//
+// function next() {
+//     if (num == 13) {
+//         // let colors = ['#914E72', '#0078BF', '#00A95C', '#3255A4', '#3D5588', '#00838A', '#BB8B41', '#407060', '#FF665E', '#925F52', '#FFE800', '#FF6C2F', '#FF48B0', '#AC936E', '#67B346', '#009DA5', '#BB76CF', '#FFB511', '#928D88', '#5EC8E5', '#82D8D5', '#FFE900', '#FF4C65', '#44D62C', '#FFFFFF']
+//         // const choose = colors[Math.floor(Math.random() * colors.length)]
+//
+//         $(".quiz_page").hide();
+//         $(".result_page").show();
+//
+//         let mbti = "";
+//
+//         $("#EI").val() < 2 ? (mbti += "I") : (mbti += "E");
+//         $("#SN").val() < 2 ? (mbti += "N") : (mbti += "S");
+//         $("#TF").val() < 2 ? (mbti += "F") : (mbti += "T");
+//         $("#JP").val() < 2 ? (mbti += "P") : (mbti += "J");
+//
+//         $("#img").attr("src", result[mbti]["img"]);
+//         $("#result").html(result[mbti]["result"]);
+//         $("#explain").html(result[mbti]["explain"]);
+//         $("#extra").html(result[mbti]["extra"]);
+//         $("#gift").html(result[mbti]["gift"]);
+//     } else {
+//         //문제 & 선택지
+//         $("#question").html(q[num]["q"]);
+//         $("#type").val(q[num]["type"]);
+//         $("#A").html(q[num]["A"]);
+//         $("#B").html(q[num]["B"]);
+//         //색 세팅
+//         $(".quiz_page").attr("style", "background-color: " + q[num]["c1"]);
+//         $("#question").css("color", q[num]["c2"]);
+//         $("#A").css("background-color", q[num]["c2"]);
+//         $("#A").css("color", q[num]["c1"]);
+//         $("#A").css("border", "3px solid" + q[num]["c1"]);
+//         $("#B").css("background-color", q[num]["c2"]);
+//         $("#B").css("color", q[num]["c1"]);
+//         $("#B").css("border", "3px solid" + q[num]["c1"]);
+//
+//         num++;
+//     }
+// }
